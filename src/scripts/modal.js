7e7b9e8e0b210b4d2d97cef6ff7e3fae591066c9
samuel <srkuran@gmail.com>
2023-03-29 04:40:28 -0300

    modal adicionado

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,10 @@
 /* Desenvolva seu código aqui... */
+function modalAction () {
+    const link = document.querySelector('.btn-cadastrar');
+    const modalContainer = document.querySelector('#modal__controller');
+
+    link.addEventListener('click', () => {
+        modalContainer.showModal();
+    })
+}
+modalAction();
